aa994bac7b0a619947a138d68b8af0c46d7a8720
het1526 <hetbhatt004@gmail.com>
2025-07-20 23:00:03 +0530

    buildReady-v1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); // new views folder
