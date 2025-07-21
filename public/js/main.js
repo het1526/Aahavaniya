27a610a9281f23df06fe5d23f4bc74ca9a39e1fd
het1526 <hetbhatt004@gmail.com>
2025-07-21 21:24:00 +0530

    Toggle btn fixed

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.querySelector(".mobile-nav-toggle");
   const navList = document.querySelector(".main-nav-list");
 
-  console.log("Toggle Button:", toggleBtn);
-  console.log("Nav List:", navList);
+  toggleBtn.addEventListener("click", () => {
+    navList.classList.toggle("show");
+  });
 });
