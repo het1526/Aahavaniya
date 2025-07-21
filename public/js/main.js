37c33e844cf46130c12f65a7d2f09ea201d7b041
het1526 <hetbhatt004@gmail.com>
2025-07-21 16:58:29 +0530

    added mobile toggle button for nav-links

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// Mobile nav-links toggle
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.querySelector(".mobile-nav-toggle");
+  const navList = document.querySelector(".main-nav-list");
+
+  console.log("Toggle Button:", toggleBtn);
+  console.log("Nav List:", navList);
+});
